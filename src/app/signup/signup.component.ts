f0071cb1bc015fd99b560ec3e088016c79993f6b
Akkitheakhil <akkitheakhil>
2019-07-03 08:01:12 +0530

    New changes for Signup Only

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -10,15 +10,20 @@ export class Register {
     teamMember5email: string;
 }
 
+export class Response {
+    status: string;
+    message: string;
+}
+
 @Component({
     selector: 'app-signup',
     templateUrl: './signup.component.html',
     styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-public registerSuccess:boolean=false;
-public success:boolean=false;
-public signupData;
+    public registerSuccess = false;
+    public success = false;
+    public signupData;
     registerData: Register;
     registerForm: FormGroup;
     constructor(private formBuilder: FormBuilder, private api: ApiserviceService) {
@@ -27,7 +32,7 @@ public signupData;
     }
 
     ngOnInit() {
-   
+
         this.registerForm = this.formBuilder.group({
             teamNameCtrl: ['', Validators.required],
             teamMember1emailCtrl: ['', [Validators.required, Validators.email]],
@@ -35,32 +40,31 @@ public signupData;
             teamMember3emailCtrl: ['', [Validators.email]],
             teamMember4emailCtrl: ['', [Validators.email]],
             teamMember5emailCtrl: ['', [Validators.email]],
-          });
+        });
     }
 
     signUp() {
-        if(this.registerForm.valid){
+        if (this.registerForm.valid) {
             this.registerData.teamName = this.registerForm.value.teamNameCtrl;
             this.registerData.teamMember1email = this.registerForm.value.teamMember1emailCtrl;
             this.registerData.teamMember2email = this.registerForm.value.teamMember2emailCtrl;
             this.registerData.teamMember3email = this.registerForm.value.teamMember3emailCtrl;
             this.registerData.teamMember4email = this.registerForm.value.teamMember4emailCtrl;
-            this.registerData.teamMember4email = this.registerForm.value.teamMember5emailCtrl;
-            
-            this.api.registerUser(this.registerData).subscribe((data)=> {
-                //Show Success...
-       
-                this.registerSuccess=true;
-                if(data){
-                    this.signupData=data;
-                    if(this.signupData[0].status=='Success'){
-                this.success=true;
-                    }else{
-                this.success=false;
+            this.registerData.teamMember5email = this.registerForm.value.teamMember5emailCtrl;
 
+            this.api.registerUser(this.registerData).subscribe((data: Response) => {
+                this.registerSuccess = true;
+                if (data) {
+                    this.signupData = data;
+                    if (data.status == 'Success') {
+                        this.success = true;
+                    } else {
+                        this.success = false;
                     }
-                }this.success=false;
-            }          
+                } else {
+                    this.success = false;
+                }
+            }
             );
         }
     }
