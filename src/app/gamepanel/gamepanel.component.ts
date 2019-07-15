b0c911ae1234693dce9d316742bd401ce6c646ac
amalskumar <focusonamal@gmail.com>
2019-07-15 16:59:25 +0530

    no message

--- src/app/gamepanel/gamepanel.component.ts
+++ src/app/gamepanel/gamepanel.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit ,Inject} from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { LandingComponent, DialogData } from 'app/landing/landing.component';
 @Component({
   selector: 'app-gamepanel',
   templateUrl: './gamepanel.component.html',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamepanelComponent implements OnInit {
 
-  constructor() { }
+desc="Lorem Ipsum has been the industry's standard dummy text ever since the 1500shanged";
+
 
   ngOnInit() {
   }
 
+
+  constructor(
+    public dialogRef: MatDialogRef<LandingComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+  Submit(){
+    this.dialogRef.close();
+  }
 }
